f25c4c48ef9b5ccdcc7b07c24df12059a1e216c3
Sang Hung <sang.hung@my.tccd.edu>
2018-03-19 19:42:33 -0500

    Updated Notes

--- Classwork/Section3/Nile/Data/ProductDatabase.cs
+++ Classwork/Section3/Nile/Data/ProductDatabase.cs
@@ -3,93 +3,61 @@
  */
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
-namespace Nile.Data.Memory
+namespace Nile.Data
 {
     /// <summary>Provides an in-memory product database.</summary>
-    public class MemoryProductDatabase : ProductDatabase
-    {
-        /// <summary>Initializes an instance of the <see cref="MemoryProductDatabase"/> class.</summary>
-        public MemoryProductDatabase()
+    public abstract class ProductDatabase : IProductDatabase
+    {        
+        /// <summary>Add a new product.</summary>
+        /// <param name="product">The product to add.</param>
+        /// <param name="message">Error message.</param>
+        /// <returns>The added product.</returns>
+        /// <remarks>
+        /// Returns an error if product is null, invalid or if a product
+        /// with the same name already exists.
+        /// </remarks>
+        public Product Add ( Product product, out string message )
         {
-            //Array version
-            //var prods = new Product[]
-            //var prods = new []
-            //    {
-            //        new Product(),
-            //        new Product()
-            //    };
-
-            //_products = new Product[25];
-            _products = new List<Product>() 
+            //Check for null
+            if (product == null)
             {
-                new Product() { Id = _nextId++, Name = "iPhone X",
-                                IsDiscontinued = true, Price = 1500, },
-                new Product() { Id = _nextId++, Name = "Windows Phone",
-                                IsDiscontinued = true, Price = 15, },
-                new Product() { Id = _nextId++, Name = "Samsung S8",
-                                IsDiscontinued = false, Price = 800 }
+                message = "Product cannot be null.";
+                return null;
             };
 
-            //var product = new Product() {
-            //    Id = _nextId++,
-            //    Name = "iPhone X",
-            //    IsDiscontinued = true,
-            //    Price = 1500,
-            //};
-            //_products.Add(product);
-
-            //product = new Product() {
-            //    Id = _nextId++,
-            //    Name = "Windows Phone",
-            //    IsDiscontinued = true,
-            //    Price = 15,
-            //};
-            //_products.Add(product);
-
-            //product = new Product {
-            //    Id = _nextId++,
-            //    Name = "Samsung S8",
-            //    IsDiscontinued = false,
-            //    Price = 800
-            //};
-            //_products.Add(product);
-        }
-        
-        protected override Product AddCore ( Product product )
-        {
-            // Clone the object
-            product.Id = _nextId++;
-            _products.Add(Clone(product));
-
-            // Return a copy
-            return product;
-        }
-
-        protected override Product GetCore( int id )
-        {
-            //for (var index = 0; index < _products.Length; ++index)
-            foreach (var product in _products)
+            //Validate product using IValidatableObject
+            //var error = product.Validate();
+            var errors = ObjectValidator.Validate(product);
+            if (errors.Count() > 0)
             {
-                if (product.Id == id)
-                    return product;
+                //Get first error
+                message = errors.ElementAt(0).ErrorMessage;
+                return null;
             };
 
-            return null;
+            // Verify unique product
+            var existing = GetProductByName(product.Name);
+            if (existing != null)
+            {
+                message = "Product already exists.";
+                return null;
+            };
+
+            message = null;
+            return AddCore(product);
         }
 
-        protected override IEnumerable<Product> GetAllCore ()
+        /// <summary>Gets all products.</summary>
+        /// <returns>The list of products.</returns>
+        public IEnumerable<Product> GetAll ()
         {
-            //Iterator syntax
-            foreach (var product in _products)
-            {
-                if (product != null)
-                    yield return Clone(product);
-            };
-        }
-        
+            return GetAllCore();
+        }        
+
+        /// <summary>Removes a product.</summary>
+        /// <param name="id">The product ID.</param>
         public void Remove ( int id )
         {
             //TODO: Return an error if id <= 0
@@ -102,6 +70,14 @@ namespace Nile.Data.Memory
             };
         }
 
+        /// <summary>Edits an existing product.</summary>
+        /// <param name="product">The product to update.</param>
+        /// <param name="message">Error message.</param>
+        /// <returns>The updated product.</returns>
+        /// <remarks>
+        /// Returns an error if product is null, invalid, product name
+        /// already exists or if the product cannot be found.
+        /// </remarks>
         public Product Update ( Product product, out string message )
         {
             //Check for null
@@ -112,6 +88,7 @@ namespace Nile.Data.Memory
             };
 
             //Validate product using IValidatableObject
+            //var error = product.Validate();
             var errors = ObjectValidator.Validate(product);
             if (errors.Count() > 0)
             {
@@ -144,6 +121,10 @@ namespace Nile.Data.Memory
             //Return a copy
             return product;
         }
+
+        protected abstract Product AddCore( Product product );
+        protected abstract IEnumerable<Product> GetAllCore();
+        protected abstract Product GetCore( int id );
 
         #region Private Members
 
@@ -178,7 +159,18 @@ namespace Nile.Data.Memory
         //}
 
         //Find a product by its ID
-        
+        private Product GetById ( int id )
+        {
+            //for (var index = 0; index < _products.Length; ++index)
+            foreach (var product in _products)
+            {
+                if (product.Id == id)
+                    return product;
+            };
+
+            return null;
+        }
+
         private Product GetProductByName ( string name )
         {
             foreach (var product in _products)

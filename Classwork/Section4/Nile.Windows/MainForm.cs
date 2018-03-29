8cd12b71b72309104fd6c94f0ee54e10f67d266f
Sang Hung <sang.hung@my.tccd.edu>
2018-03-28 20:37:32 -0500

    Update Notes

--- Classwork/Section4/Nile.Windows/MainForm.cs
+++ Classwork/Section4/Nile.Windows/MainForm.cs
@@ -8,6 +8,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Windows.Forms;
 using Nile.Data;
+using Nile.Data.IO;
 using Nile.Data.Memory;
 
 namespace Nile.Windows
@@ -22,6 +23,8 @@ namespace Nile.Windows
         protected override void OnLoad( EventArgs e )
         {
             base.OnLoad(e);
+
+            _database = new FileProductDatabase("products.csv");
 
             RefreshUI();
         }
@@ -73,9 +76,17 @@ namespace Nile.Windows
                 return;
 
             //Add to database
-            _database.Add(form.Product, out var message);
-            if (!String.IsNullOrEmpty(message))
-                MessageBox.Show(message);
+            //_database.Add(form.Product);
+            try
+            {
+                _database.Add(null);
+            } catch (NotImplementedException)
+            {
+                MessageBox.Show("not implemented yet");
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            };
 
             RefreshUI();
         }
@@ -91,7 +102,7 @@ namespace Nile.Windows
                 return;
             };
 
-            EditProduct(product);
+            EditProduct(product);            
         }
 
         private void OnProductRemove( object sender, EventArgs e )
@@ -123,7 +134,13 @@ namespace Nile.Windows
                 return;
 
             //Remove product
-            _database.Remove(product.Id);
+            try
+            {
+                _database.Remove(product.Id);
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            };
 
             RefreshUI();
         }
@@ -138,9 +155,14 @@ namespace Nile.Windows
 
             //Update the product
             form.Product.Id = product.Id;
-            _database.Update(form.Product, out var message);
-            if (!String.IsNullOrEmpty(message))
-                MessageBox.Show(message);
+
+            try
+            {
+                _database.Update(form.Product, out var message);
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            };
 
             RefreshUI();
         }
@@ -168,7 +190,7 @@ namespace Nile.Windows
                            == DialogResult.Yes;
         }
 
-        private IProductDatabase _database = new MemoryProductDatabase();
+        private IProductDatabase _database;
 
         #endregion
     }
